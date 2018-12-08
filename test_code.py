7a7eb57beb2199bb56b2c2b66230315a940ebdad
wexnert <wexnert@sas.upenn.edu>
2018-12-08 01:27:12 -0500

    Updated code to test SpaCyParser and WordSubstitutor

--- test_code.py
+++ test_code.py
@@ -6,33 +6,38 @@ Tester code to generate a set of replacements in a sentence
 from DataMuseQuerier import DataMuseQuerier
 from Word import Word
 from WordSubstitutor import WordSubstitutor
+from SpaCyParser import SpaCyParser
+import re
 
 querier = DataMuseQuerier()
-test_sentence = "Wow you threw that ball very far"
-#test_sentence = "I ate a delicious and big ham sandwich with cheese for lunch"
-# WORRY ABOUT PUNCTUATION AND PARSING LATER
-sentence_vector = test_sentence.split(' ')
-# get left and right contexts
-left_contexts = [""]*len(sentence_vector)
-for i in range(1,len(sentence_vector)):
-    left_contexts[i] = sentence_vector[i-1]
-    
-right_contexts = [""]*len(sentence_vector)
-for i in range(0, len(sentence_vector)-1):
-    right_contexts[i] = sentence_vector[i+1]
-
-parts_of_speech = ['adj', 'pron', 'v', 'art', 'n', 'adv', 'adv']
-#parts_of_speech = ['pron', 'v', 'art', 'adj', 'conj', 'adj', 'n', 'n', 'prep', 'n', 'art', 'n']
-output_vec = []
+parser = SpaCyParser()
 wsub = WordSubstitutor()
-for i in range(len(sentence_vector)):
-    word = Word(sentence_vector[i], parts_of_speech[i])
-    word.set_frequency(querier.get_frequency(word))
+
+#test_sentences = "Wow you threw that ball very far. \
+#                  I ate a delicious and big ham sandwich with cheese for lunch \
+#                  The pretty girl was very worried about the difficult final exams coming up.\
+#                  She would like more than anything to take a nap."
+
+output_vec = []
+parsed_words_and_tags = parser.parse_and_tag_text(test_sentences)
+for token in parsed_words_and_tags:
+    word = Word(token[0], token[1])
     #output_vec.append(wsub.get_best_synonym(word, left_contexts[i], right_contexts[i]).get_word())
     output_vec.append(wsub.get_best_synonym(word).get_word())
 
-    
 print(output_vec)
 
+# Now concatinate:
+output_text = output_vec[0]
+non_character = re.compile('\W')
+for token in output_vec[1:]:
+    # Don't add a space before character if punctuation
+    if re.match(non_character, token):
+        output_text = output_text + token
+    
+    # If the token is a number or word, add a space before
+    else:
+        output_text = output_text + ' ' + token
+    
 
-
+print(output_text)
